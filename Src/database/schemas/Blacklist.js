93b44446a2b0b027ae319afad159dd6b320b4dd6
NedcloarBR <nedcloar1@hotmail.com>
2021-01-19 14:12:29 -0300

    Deletando alguns arquivos pra desbugar o projeto

--- Src/database/schemas/Blacklist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const BlacklistSchema = mongoose.Schema({
-    User: String,
-    Blacklist: Boolean
-});
-
-module.exports = mongoose.model('Blacklist', BlacklistSchema);
--- Src/database/schemas/DisableCommands.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DisableCommandsSchema = mongoose.Schema({
-    //_id: Schema.Types.ObjectId,
-    name: String,
-    cmds: Array
-});
-
-module.exports = mongoose.model('DisableCommands', DisableCommandsSchema);
