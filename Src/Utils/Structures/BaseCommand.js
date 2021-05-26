29d84f8dfbce0cabb923de1b89e954b2c69b61b3
NedcloarBR <nedcloar1@hotmail.com>
2021-05-26 17:56:27 -0300

    Update BaseEvent and BaseCommand

--- Src/Utils/Structures/BaseCommand.js
+++ Src/Utils/Structures/BaseCommand.js
@@ -1,20 +1,16 @@
-const { Util } = require('discord.js');
 const { Permissions } = require('discord.js');
-
-const DEFAULT_RATELIMIT = {
-	reset: 5000,
-	bucket: 1,
-	stack: true
-};
 module.exports = class BaseCommand {
 
 	constructor(client, name, options = {}) {
 		this.client = client;
 		this.name = options.name || name;
 		this.aliases = options.aliases || [];
-		this.description = options.description || 'No description provided.';
+		this.description = options.description || 'Nenhuma descrição';
 		this.category = options.category || 'General';
 		this.usage = `${this.client.prefix}${this.name} ${options.usage || ''}`.trim();
+		this.examples = options.examples || [];
+        this.disabled = options.disabled || false;
+        this.cooldown = "cooldown" in options ? options.cooldown : 5 || 5;
 		this.userPerms = new Permissions(options.userPerms).freeze();
 		this.botPerms = new Permissions(options.botPerms).freeze();
 		this.guildOnly = options.guildOnly || false;
@@ -22,11 +18,13 @@ module.exports = class BaseCommand {
 		this.ownerOnly = options.ownerOnly || false;
 		this.nsfw = options.nsfw || false;
 		this.args = options.args || false;
-		this.ratelimit = Util.mergeDefault(DEFAULT_RATELIMIT, options.ratelimit || DEFAULT_RATELIMIT);
 	}
 
 	async run(message, args) {
 		throw new Error(`Comando \`${this.name}\` Não proveu um método Run!`);
 	}
 
+	reload() {
+        return this.store.load(this.file.path);
+    }
 };
--- Src/Utils/Structures/BaseEvent.js
+++ Src/Utils/Structures/BaseEvent.js
@@ -1,5 +1,17 @@
 module.exports = class BaseEvent {
-    constructor(name) {
-      this.name = name;
-    }
+  constructor(client, name, options = {}) {
+    this.name = options.name || name;
+    this.partials = ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER', 'GUILD_MESSAGE_REACTIONS'],
+    this.client = client || options.client;
+    this.type = options.once ? 'once' : 'on';
+    this.emitter = (typeof options.emitter === 'string' ? this.client[options.emitter] : options.emitter) || this.client;
   }
+
+  async run(client, options) {
+    throw new Error(`Um método Run não foi implementado em: ${this.name}`);
+  }
+
+  reload() {
+    return this.store.load(this.file.path);
+  }
+}
