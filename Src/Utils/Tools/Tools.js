ec8a3c7d54e0e6f5882d9c71f591540f457cf7cc
NedcloarBR <nedcloar1@hotmail.com>
2021-05-26 18:01:09 -0300

    Update some Tools

--- Src/Utils/Tools/Tools.js
+++ Src/Utils/Tools/Tools.js
@@ -176,9 +176,9 @@ module.exports = class Tools {
         { name: "Minecraft", type: "PLAYING" },
         { name: "GTA V", type: "PLAYING" },
         { name: "Rocket League", type: "PLAYING" },
-        { name: "League Of Legends", type: "STREAMING", url: "https://twitch.tv/Nedcloar_BR" },
-        { name: "VALORANT", type: "STREAMING", url: "https://twitch.tv/Nedcloar_BR" },
-        { name: "Terraria", type: "STREAMING", url: "https://twitch.tv/Nedcloar_BR" },
+        { name: "League Of Legends", type: "STREAMING", url: "https://twitch.tv/NedcloarBR" },
+        { name: "VALORANT", type: "STREAMING", url: "https://twitch.tv/NedcloarBR" },
+        { name: "Terraria", type: "STREAMING", url: "https://twitch.tv/NedcloarBR" },
     ]
 
     flags = {
