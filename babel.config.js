4bfb7f4f441bf9561a48669322b69373c1e8540b
NedcloarBR <nedcloar1@hotmail.com>
2021-08-25 17:56:01 -0300

    Configure Babel

---
+++ babel.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable linebreak-style */
+module.exports = {
+  presets: [['@babel/preset-env',
+    {
+      targets: {
+        node: 'current',
+      },
+    },
+  ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@/*': './Src/*',
+        '@Client': './Src/Client/*',
+        '@Structures/*': './Src/Utils/Structures/*',
+        '@Handlers/*': './Src/Utils/Handlers/*',
+        '@Schema/*': './Src/Database/Schema/*',
+        '@Utils/*': './Src/Utils/*',
+        '@Tools/*': './Src/Utils/Tools/*',
+        '@Commands/*': './Src/Commands/*',
+        '@Languages/*': './Src/Utils/Languages/*',
+        '@Config/*': './Src/Config/*',
+        '@Types/*': './Src/Types/*',
+      },
+    }],
+  ],
+  ignore: [
+    '**/*.spec.ts',
+  ],
+};
