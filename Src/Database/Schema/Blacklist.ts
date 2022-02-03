f304a0629046a734b3cf2c3d7981577af44bd416
NedcloarBR <nedcloar1@hotmail.com>
2022-02-03 14:02:09 -0300

    Configure ESlint, Prettier, Babel, TS(Pre-Rewrite)

--- Src/Database/Schema/Blacklist.ts
+++
@@ -1,8 +0,0 @@
-import * as Mongoose from "mongoose";
-const BlacklistSchema: Mongoose.Schema = new Mongoose.Schema({
-  User: String,
-  Blacklist: Boolean,
-});
-
-const Blacklist = Mongoose.model("Blacklist", BlacklistSchema);
-export default Blacklist;
