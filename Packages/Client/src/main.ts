077c88d67cb03cb2466df490f89dbe11df23823d
NedcloarBR <nedcloar1@hotmail.com>
2024-03-30 14:58:02 -0300

    feat(api): use fastify

--- Packages/Client/src/main.ts
+++ Packages/Client/src/main.ts
@@ -2,6 +2,7 @@ import { AppModule } from "@/app.module";
 import { Logger, ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { name } from "../package.json";
 import { CommandInterceptor } from "./common/interceptors/Command.interceptor";
 import { ShardingManager, TopGGAutoPoster } from "./lib";
@@ -9,7 +10,7 @@ import { Config } from "./modules/config/types";
 
 async function bootstrap() {
 	const logger = new Logger("Main");
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 	const configService = app.get<ConfigService>(ConfigService);
 	const Port = configService.get<number>("PORT");
 
@@ -27,11 +28,11 @@ async function bootstrap() {
 	const TopGGPoster = new TopGGAutoPoster(configService.getOrThrow<Config["TopGGToken"]>("TopGGToken"), ShardManager);
 
 	try {
-		await app.listen(Port);
+		await app.listen(Port, "0.0.0.0");
 		await ShardManager.init();
 		await TopGGPoster.init();
 
-		logger.log(`${name} Running on Port: ${Port} in ${process.env.ENVIRONMENT} mode`);
+		logger.log(`${name} Running on Port: ${Port} in ${process.env.ENVIRONMENT} mode | URL: ${await app.getUrl()}`);
 	} catch (error) {
 		logger.error("An error occurred when starting: ", (error as Error).message);
 	}
