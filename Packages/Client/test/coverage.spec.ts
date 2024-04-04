175912df0a7483c3e1aef5bed7ab8b6a6f175bd5
NedcloarBR <nedcloar1@hotmail.com>
2024-04-04 18:12:31 -0300

    test: force ci actions

--- Packages/Client/test/coverage.spec.ts
+++ Packages/Client/test/coverage.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
 
-describe("Generate Coverage", async () => {
+describe("Generate Coverage", () => {
 	it('Should return a "Hello, World!"', () => {
 		const Test = "Hello, World!";
 		expect(Test).toBe("Hello, World!");
