3b29c3c63f583fc63b87d6ee9990650148ffb2ed
NedcloarBR <nedcloar1@hotmail.com>
2024-04-06 19:58:45 -0300

    ci: test Coverage workflow

--- Packages/Client/test/coverage.spec.ts
+++ Packages/Client/test/coverage.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 
 describe("Generate Coverage", () => {
-	it('Should return a "Hello, World!"', async () => {
+	it('Should return a "Hello, World!"', () => {
 		const Test = "Hello, World!";
 		expect(Test).toBe("Hello, World!");
 	});
