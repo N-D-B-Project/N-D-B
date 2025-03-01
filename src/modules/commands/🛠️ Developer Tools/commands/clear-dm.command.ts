0c2110db0c51704c84bf2cc088cf399ccf1b654e
NedcloarBR <nedcloar1@hotmail.com>
2025-03-01 11:09:47 -0300

    feat: add clear dm command

---
+++ src/modules/commands/🛠️ Developer Tools/commands/clear-dm.command.ts
@@ -0,0 +1,40 @@
+import { CommandConfig, CommandPermissions } from "@/common/decorators/";
+import { Logger } from "@nestjs/common";
+import { ApplicationIntegrationType, InteractionContextType } from "discord.js";
+import { Ctx, type SlashCommandContext, Subcommand } from "necord";
+import { DeveloperToolsCommand } from "../DeveloperTools.decorator";
+
+@DeveloperToolsCommand()
+export class ClearDMCommand {
+  private readonly logger = new Logger(ClearDMCommand.name);
+
+  @Subcommand({
+    name: "clear_dm",
+    description: "clear bot's dm",
+    contexts: [
+      InteractionContextType.BotDM,
+    ],
+    integrationTypes: [
+      ApplicationIntegrationType.UserInstall,
+    ],
+  })
+  @CommandConfig({ category: "🛠️ Developer Tools", disable: false })
+  @CommandPermissions({
+    user: [],
+    bot: [],
+    guildOnly: false,
+    testOnly: false,
+    ownerOnly: false,
+  })
+  public async onCommandRun(@Ctx() [interaction]: SlashCommandContext) {
+    const channel = interaction.user.dmChannel
+    const messages = await channel.messages.fetch({
+      limit: 100
+    });
+    for await(const message of messages.values()) {
+      if(message.deletable && message.author.bot) {
+        await message.delete()
+      }
+    }
+  }
+}
