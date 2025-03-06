871429b4afa10d275ac510d81fd4c7d1e0e4092d
NedcloarBR <nedcloar1@hotmail.com>
2025-03-06 15:50:42 -0300

    types: remove related to api

--- src/types/Constants.ts
+++ src/types/Constants.ts
@@ -1,16 +1,9 @@
-import { GatewayVersion } from "discord-api-types/gateway";
-export const DiscordAPIUrl = `https://discord.com/api/v${GatewayVersion}`;
-
 export const isInProduction = false;
 
 export enum Services {
 	NDB = "NDB_SERVICE",
 	Prisma = "PRISMA_SERVICE",
 	Database = "DATABASE_SERVICE",
-	User = "USER_SERVICE",
-	Auth = "AUTH_SERVICE",
-	Discord = "DISCORD_SERVICE",
-	Discord_HTTP = "DISCORD_HTTP_SERVICE",
 }
 
 export enum Extends {
