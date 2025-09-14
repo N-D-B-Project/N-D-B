198820dd6edc601996b07d60602595625bacb500
NedcloarBR <nedcloar1@hotmail.com>
2025-09-14 15:48:15 -0300

    fix: add missing translations

--- src/modules/tickets/commands/tickets.decorator.ts
+++ src/modules/tickets/commands/tickets.decorator.ts
@@ -4,7 +4,9 @@ import { createCommandGroupDecorator } from "necord";
 export const TicketCommand = createCommandGroupDecorator({
 	name: "tickets",
 	description: "Category 🎫 Tickets",
-	// nameLocalizations: localizationMapByKey("Tickets.category.name"),
-	// descriptionLocalizations: localizationMapByKey("Tickets.category.description"),
+	nameLocalizations: localizationMapByKey("Tickets.category.name"),
+	descriptionLocalizations: localizationMapByKey(
+		"Tickets.category.description",
+	),
 	dmPermission: false,
 });
